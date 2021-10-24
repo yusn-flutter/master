dd9ca152f664bd8fbc7ec2ad448a7077ef6bcd9a
yusn-star <yusnongh3412.0983@gmail.com>
2021-10-24 22:13:32 +0900

    myaoo 初回コミット

---
+++ android/app/src/main/java/com/example/myaoo/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.myaoo;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: RandomWords(), // 画面全体
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  _RandomWords createState() => _RandomWords();
+}
+
+class _RandomWords extends State<RandomWords> {
+  final wordPairList = <WordPair>[];
+  final saved = <WordPair>{};
+  final font = const TextStyle(fontSize: 18.0);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome to Flutter'),
+      ),
+      body: createWordPairList(),
+    );
+  }
+
+  Widget createWordPairList() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider();
+
+        final index = i~/2;
+        if (index >= wordPairList.length) {
+          wordPairList.addAll(generateWordPairs().take(10));
+        }
+        return buildRow(wordPairList[index]);
+      }
+    );
+  }
+
+  Widget buildRow(WordPair pair) {
+    final alreadySaved = saved.contains(pair);
+    return ListTile(
+      // listの先頭
+      leading: Icon(
+        Icons.accessibility_new,
+      ),
+      // listの中央
+      title: Text(
+        pair.asPascalCase,
+        style: font,
+      ),
+      // titleの下
+      subtitle: Text(
+        'subtitleを記入',
+        style: TextStyle(fontSize: 9.0),
+      ),
+      // listの末尾
+      trailing: Icon(
+        // 三項演算子
+        // alreadySavedがtrueの場合左側、falseの場合右側
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      // タップ時の処理
+      onTap: () {
+        // 状態(State)の変更処理
+        setState(() {
+          alreadySaved ? saved.remove(pair) : saved.add(pair);
+        });
+      },
+    );
+  }
+}
